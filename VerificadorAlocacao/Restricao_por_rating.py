92953dd51cfde2fcefe0999fceaf8becec429c65
cicerolibardi@gmail.com <cicerolibardi@gmail.com>
2025-01-21 00:39:06 +0000

    Adiciona validação de restrição por rating ainda sem integração com arquivos CSV e sem integrar com a régua implementada pelo João

--- VerificadorAlocacao/Restricao_por_rating.py
+++ VerificadorAlocacao/Restricao_por_rating.py
@@ -1,4 +1,6 @@
 # Databricks notebook source
+import pandas as pd
+
 def primeiro_numero_e_posicao(string:str)->tuple[int,int]:
     for i,char in enumerate(string):
         if char.isnumeric():
@@ -35,7 +37,7 @@ def get_ratings_igual_abaixo(rating:str, tabela_car_fundo:str)->list[str]:
     lista_nivels = []
     for row in df_ratings:
         #print(row["IntervaloRating"],row["Nivel"])
-        lista_ratings.append(row["IntervaloRating"])
+        lista_ratings.append(row["RatingKinea"]) # Trocar para IntervaloRating
         lista_nivels.append(row["Nivel"])
     
     ratings_dict = construir_dict_ratings(lista_ratings,lista_nivels)
@@ -48,7 +50,7 @@ def get_ratings_igual_abaixo(rating:str, tabela_car_fundo:str)->list[str]:
     if best_level == -1:
         raise RuntimeError("Não foi possível achar nível do rating")
     
-    print(best_level)
+    # print(best_level)
     return dict(
         filter(lambda x: x[0] >= best_level,ratings_dict.items()) #essa equação ou inequação é a principal coisa a se mudar caso precise modificar a lógica
     )
@@ -73,30 +75,17 @@ mapeamento_fundos_tabela_car = {
 # FUNDO 389 não tem na tabela dos fundos que apareceram em desafio_kinea.boletagem_cp.fundostipocar
 lista_fundos = ["KAT", "ID2", "FRA", "APP", "PAL", "652", "PDA", "APO", "PID", "678", "KRF", "KCP", "BVP", "RFA", "KOP", "134", "CPI", "846", "IRF"]
 
-dict_fundos_tipoCAR = {}
-
-for fundo in lista_fundos:
-    dict_fundos_tipoCAR[fundo] = spark.sql(f"SELECT DISTINCT TipoCAR FROM desafio_kinea.boletagem_cp.fundostipocar WHERE Fundo = '{fundo}'").collect()[0][0]
-
-fundo_teste = lista_fundos[2]
-
-ratings_igual_abaixo = get_ratings_igual_abaixo("Aaa", mapeamento_fundos_tabela_car[dict_fundos_tipoCAR[fundo_teste]])
-
-ratings = []
-
-for key,val in ratings_igual_abaixo.items():
-    for rating in val:
-        ratings.append(rating)
-
 
 
 # COMMAND ----------
 
+from typing import List
 
 # O objetivo dessa query é ditar qual a porcentagem do PL de certo fundo é composto por ativos de crédito privado de até certo rating, de acordo com a lógica das tabelas CAR
 
-query_sum_positions_por_fundo = f"""
-WITH pl_total_fundo AS (
+def query_fundo_sum_position(ratings:List[str])->pd.DataFrame:
+  query_sum_positions_por_fundo = f"""
+  WITH pl_total_fundo AS (
   SELECT t1.Codigo, t1.Data, t1.PL
   FROM desafio_kinea.boletagem_cp.cotas as t1
   JOIN (
@@ -152,33 +141,103 @@ tab_fundos AS (
    AND tab_pl.TradingDesk = tab_booksoverview.TradingDesk
 ) --retorna ativos de credito privado 
 
-SELECT  t1.TradingDesk, SUM(Position) FROM tab_fundos as t1 --soma e acha o total de crédito privado de cada fundo
-JOIN --join na tabela de data mais recente, filtrando a tabela para as combinações de cada fundo - ativo mais recentes
-(
-  -- acha a data mais recente para cada combinação fundo e ativo
-  SELECT TradingDesk, Product, MAX(PositionDate) AS MaxData from tab_fundos
-  GROUP BY TradingDesk, Product
-) t2
-ON t1.TradingDesk = t2.TradingDesk AND t1.Product = t2.Product AND t1.PositionDate = t2.MaxData
-JOIN --subquery para o rating dos atiivos
-(
-  SELECT DISTINCT 
-    Emissor, 
-    Ativo, 
-    FLOOR(DATEDIFF(Vencimento,CURRENT_DATE) / 365) AS ExpiracaoAnos,
-    RatingOp,
-    RatingGrupo 
-  FROM desafio_kinea.boletagem_cp.agendacp
-  JOIN desafio_kinea.boletagem_cp.cadastroativo ON TickerOp = Ativo --join para ter a coluna de Vencimento
-  JOIN desafio_kinea.boletagem_cp.ratingopatual ON ratingopatual.TickerOp = Ativo--join para ter coluna de rating
-  JOIN desafio_kinea.boletagem_cp.ratinggrupoatual ON NomeGrupo = Emissor 
-)
-ON  t1.Product = Ativo
-WHERE RatingOp IN ({','.join(map(repr, ratings))}) --filtra pelo rating
-GROUP BY t1.TradingDesk"""
 
-a = spark.sql(query_sum_positions_por_fundo)
-a.show()
+SELECT tabela_emissor.TradingDesk, PL as pl_total , pl_credito_privado FROM pl_total_fundo
+JOIN
+(
+    SELECT  t1.TradingDesk, SUM(Position) as pl_credito_privado FROM tab_fundos as t1 --soma e acha o total de crédito privado de cada fundo
+    JOIN --join na tabela de data mais recente, filtrando a tabela para as combinações de cada fundo - ativo mais recentes
+    (
+      -- acha a data mais recente para cada combinação fundo e ativo
+      SELECT TradingDesk, Product, MAX(PositionDate) AS MaxData from tab_fundos
+      GROUP BY TradingDesk, Product
+    ) t2
+    ON t1.TradingDesk = t2.TradingDesk AND t1.Product = t2.Product AND t1.PositionDate = t2.MaxData
+    JOIN --subquery para o rating dos atiivos
+    (
+      SELECT DISTINCT 
+        Emissor, 
+        Ativo, 
+        FLOOR(DATEDIFF(Vencimento,CURRENT_DATE) / 365) AS ExpiracaoAnos,
+        RatingOp,
+        RatingGrupo 
+      FROM desafio_kinea.boletagem_cp.agendacp
+      JOIN desafio_kinea.boletagem_cp.cadastroativo ON TickerOp = Ativo --join para ter a coluna de Vencimento
+      JOIN desafio_kinea.boletagem_cp.ratingopatual ON ratingopatual.TickerOp = Ativo--join para ter coluna de rating
+      JOIN desafio_kinea.boletagem_cp.ratinggrupoatual ON NomeGrupo = Emissor 
+    )
+    ON  t1.Product = Ativo
+    WHERE RatingOp IN ({','.join(map(repr, ratings))}) --filtra pelo rating
+    GROUP BY t1.TradingDesk
+) AS tabela_emissor
+ON pl_total_fundo.Codigo = tabela_emissor.TradingDesk"""
+
+  return spark.sql(query_sum_positions_por_fundo)
+
+# COMMAND ----------
+
+#Pegar a régua que foi dada pelo João
+
+# df de [Fundo | alocacao], código do fundo
+# ["KAT", "ID2", "FRA", "APP", "PAL", "652", "PDA", "APO", "PID", "678", "KRF", "KCP", "BVP", "RFA", "KOP", "134", "CPI", "846", "IRF"]
+
+# TODO Comentar e melhorar código
+
+# TODO Integrar com as tabelas CSV implementadas
+
+# TODO Integrar com o que o João fez 
+ativo = 'ENMTA4'
+
+fundo_dist_regua = {
+    'fundo': ['RFA', 'APO', 'ID2'],
+    'valor': [sum([0.4753, 0.2623, 0.3275]),
+              sum([0.1017, 0.3478, 0.1845]),
+              sum([0.4230, 0.3899, 0.4879])]
+}
+df_regua_fundo_valor = pd.DataFrame(fundo_dist_regua)
+
+#Buscar qual é o rating do fundo | RatingOp
+
+caminho_base = "../ScriptsSQL"
+caminho = caminho_base +'/' + 'tabelascar_info_ativos' + '.sql'
+with open(caminho, 'r') as f:
+    query = f.read()
+df_ativos = spark.sql(query).toPandas()
+
+df_regua_fundo_valor = pd.DataFrame(fundo_dist_regua)
+
+dict_fundos_tipoCAR = {}
+for fundo in fundo_dist_regua["fundo"]:
+    dict_fundos_tipoCAR[fundo] = spark.sql(f"SELECT DISTINCT TipoCAR FROM desafio_kinea.boletagem_cp.fundostipocar WHERE Fundo = '{fundo}'").collect()[0][0]
+
+for fundo in df_regua_fundo_valor["fundo"]:
+    tabela_car_fundo = spark.sql(f"select * from {mapeamento_fundos_tabela_car[dict_fundos_tipoCAR[fundo]]}").toPandas()
+    
+    ratings_igual_abaixo = get_ratings_igual_abaixo(df_ativos[df_ativos["Ativo"] == ativo]["RatingOp"].values[0],
+                                                    mapeamento_fundos_tabela_car[dict_fundos_tipoCAR[fundo]])
+    
+    ratings = []
+    for key,val in ratings_igual_abaixo.items():
+        for rating in val:
+            ratings.append(rating)
+
+    fundo_sum_position = query_fundo_sum_position(ratings).toPandas()
+
+    fundo_sum_position["porcentagem_pl"] = fundo_sum_position["pl_credito_privado"] / fundo_sum_position["pl_total"]
+
+    fundo_porcentagem_pl = fundo_sum_position[fundo_sum_position["TradingDesk"] == fundo]["porcentagem_pl"].values[0]
+
+    tabela_car_max_pl = tabela_car_fundo[tabela_car_fundo["Nivel"] == int(min(ratings_igual_abaixo.keys()))]["MaxPL"].values[0]
+
+    if fundo_porcentagem_pl > tabela_car_max_pl:
+        # TODO se entrar, deve fazer a construção do DF de realocação.
+        print("entrou aqui")
+    else:
+        # Printando a porcetagem à titulo de curiosidade
+        print(fundo, fundo_porcentagem_pl)
+        print(fundo, tabela_car_max_pl)
+        print("eh isto aí")
+
 
 # COMMAND ----------
 
