59d38472b8986c1517f4fd0764f9250dda257b8a
caue-paiva <caue-paiva>
2025-01-26 21:04:45 +0000

    "Renomeia o arquivo py de calculo de regua e cria um novo para processar uma ordem"

---
+++ AplicacaoFinal/ProcessaOrdem.py
@@ -0,0 +1,3 @@
+"""
+Vamos ter uma task com trigger de um upload de um CSV no folder /volumes/ordems e a task irá executar esse código, que irá chamar a função de calcular regua para cada ativo da ordem
+"""
