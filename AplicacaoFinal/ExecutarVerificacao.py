20701c2b582b9a03e06c831f19b36b9696c7ad49
caue-paiva <caue-paiva>
2025-01-28 18:27:42 +0000

    "Ideia geral e comentário das funções de " ExecutarOtimizacaoInicial" e "ExecutarOtimizacaoCiclo" foram colocadas no ExecutarVerificao.py

--- AplicacaoFinal/ExecutarVerificacao.py
+++ AplicacaoFinal/ExecutarVerificacao.py
@@ -3,14 +3,33 @@ Script para executar Verificações
 """
 from VerificadorTabelacar import  VerificadorTabelacar
 from DadosAlocacao import DadosAlocacao
+from Otimizacao import otimiza_regua #essa função recebe 2 df como arg
 import pandas as pd
 
 def ExecutarVerificacoes():
+    """
+    Com as réguas iniciais dos ativos de uma dada ordem salvas no volume do desafio /Reguas/, faz a verificação de acordo com as regras das tabelacars de cada régua e salva um DF de verificacao 
+    no folder /VerificacaoTabelasCar/.
+    """
     verificador = VerificadorTabelacar()
     for i in dbutils.fs.ls("/Volumes/desafio_kinea/boletagem_cp/files/Reguas/"): #para cada régua de ativo nesse path, vamos passar a régua inicial pelo verificador e salvar o resultado em um CSV
         ativo:str = i.name.split("_")[1].removesuffix(".csv") #pega nome do ativo pelo 
         alocacao = pd.read_csv(i.path.removeprefix("dbfs:")).drop(columns='book')
         resultado_verificacao = verificador.verifica_alocacao(alocacao,ativo)
         resultado_verificacao.to_csv(f"/Volumes/desafio_kinea/boletagem_cp/files/VerificacaoTabelasCar/VERIFICACAO_{ativo}.csv",index=False)
+
+def ExecutarOtimizacaoInicial():
+    """
+    Com as réguas iniciais dos ativos de uma dada ordem salvas no volume do desafio /Reguas/, faz a verificacao de acordo com as tabelascar e depois otimizar de acordo com a regua e restrições e
+    salvar as reguas otimizadas no diretório X do volume. Essa função realiza a primeira otimização no ciclo de processa uma ordem
+    """
+
+def ExecutarOtimizacaoCiclo():
+    """
+    Com uma ou mais reguas otimizadas de uma ordem, realiza mais um processo de verificacao e otimizacao caso necessário. As reguas anteriores são lidas do path X (path de reguas otimizadas) do volumes e depois de atualizadas denovo, são salvas nesse mesmo path, substituindo as réguas otimizadas anteriores.
+    """
+
+
+    
 if __name__ == '__main__':
     regua = ExecutarVerificacoes()
--- AplicacaoFinal/ProcessaOrdem.py
+++ AplicacaoFinal/ProcessaOrdem.py
@@ -4,15 +4,19 @@ Vamos ter uma task com trigger de um upload de um CSV no folder /volumes/ordems 
 import pandas as pd
 from pathlib import Path
 from CalculoReguaMacro import ReguaMacro
-class ProcessaOrdem:
-    def __init__(self):
+class ProcessaOrdem: 
+    """
+    Classe que ao ser instanciada processa a ordem mais atual no volume de arquivos do desafio no path /Ordem/, salvando ela como um df.
+    Essa classe tem o método processar_ordem que irá processar a ordem  e chamar a função de calcular régua para cada ativo da ordem, salvando as réguas no volume do desafio
+    """
+    def __init__(self): 
         self.input_path = Path("/Volumes/desafio_kinea/boletagem_cp/files/Ordem/Ordem.csv")
         self.ordem = pd.read_csv(self.input_path)
 
-    def processar_ordem(self):
+    def processar_ordem(self): #
         for index, row in self.ordem.iterrows():
             regua = ReguaMacro()
-            regua.calcula_regua(row["ativo"],True)
+            regua.calcula_regua(row["ativo"],True) #calcula régua e salva no volume
 
 if __name__ == "__main__":
     ordem = ProcessaOrdem()
