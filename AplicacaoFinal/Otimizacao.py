de84a049d4f69e628c5630b85b314fa4adea7988
JpABrasil <JpABrasil>
2025-01-30 15:49:10 +0000

    Atualizando Otimização

--- AplicacaoFinal/Otimizacao.py
+++ AplicacaoFinal/Otimizacao.py
@@ -8,42 +8,50 @@ def otimiza_regua(regua_ideal:pd.DataFrame, diferencas:pd.DataFrame)->tuple[pd.D
     """
     Dado uma DF régua ideal de alocação de um ativo em vários fundos e um DF diferenças que dita qual a diferença entre a alocação ideal vs as restrições  das tabelasCar (negativo que dizer acima do possível), 
     otimiza a regua ideia de acordo com as diferenças, de forma a ter a melhor alocação possível dentro das regras.
-
     Args:
         regua_ideal (pd.DataFrame): DF com as alocações ideais de cada fundo
         diferencas (pd.DataFrame): DF com as diferenças entre a alocação ideal e a alocação real, com uma diferença negativa (real ou porcentagem) ditando que a alocação passou do limite de certa verificação da tabelacar
     Return:
         (tuple[pd.DataFrame,float]): tupla com o DF da régua otimizada e o erro da otimização
     """
-
     #Inicia uma Régua com os valores distribuidos uniformemente entre os fundos
     x0 = np.array([1/len(regua_ideal)] * len(regua_ideal))
 
+    #Calculando Limite Máximo de Cada Fundo
+    #limites_maximos =regua_ideal['percentual_alocacao'] + diferencas[['diferenca_porcentagem_ano','diferenca_porcentagem_emissor','diferenca_porcentagem_pl_privado']].min(axis=1)
+    limites_maximos =regua_ideal['percentual_alocacao'] + regua_ideal['percentual_alocacao'] * diferencas[['diferenca_porcentagem_ano','diferenca_porcentagem_emissor','diferenca_porcentagem_pl_privado']].min(axis=1)
+
+    #Adicionando Bounds
+    bounds = []
+    for limite in limites_maximos:
+        bounds.append((0,limite))
+
     #Função Objetivo -> Soma dos Módulos das diferenças entre a Régua Ideal e a Régua Possível
     def objetivo(x, regua_ideal):
-        return np.power(x - regua_ideal['percentual_alocacao'], 2).sum()
+        y = np.abs(x -regua_ideal['percentual_alocacao']).sum()
+        return y
+    
+    #Restrição para Limite Máximo
+    def limite_maximo(x, i, limite):
+        #print(f"Verificando restrição para i={i}: limite={l}, valor atual={x[i]}")
+        return limite - x[i]
+    
+    #Restrição Soma das Alocações Igual a 1
+    def soma_1(x):
+        print(sum(x))
+        return sum(x) - 1
 
-    #Contraints Iniciais
-    constraints=[
-    {'type': 'eq', 'fun': lambda x: sum(x) - 1} #A soma dos valores da régua tem que ser igual a um
-    ]
+    #Restrição para cada alocação ser maior ou igual a 0
+    def alocacao_maiorque_0(x,i): 
+        return x[i]
+    
+    #Lista com Restrições
+    constraints = []
+    #constraints.append({'type': 'eq', 'fun': lambda x: soma_1(x)})
     for i in range(len(x0)):
-        constraints.append({'type': 'ineq', 'fun': lambda x: x[i]}) #Todos os valores de x tem que ser maiores que 0
-
-    #Tratar Dataframe de Diferenças
-
-    #Transformar Diferenças em Constraints
-    limites = {}
-    diferencas = diferencas.reset_index(drop=True)
-    colunas_valores = list(filter(lambda x: "diferenca" in x ,diferencas.columns)) #filtra penas colunas do DF de diferença (percentual e total)
-
-    for index,row in diferencas.iterrows():
-        limite_fundo = []
-        for col in ['diferenca_total_ano', 'diferenca_total_emissor', 'diferenca_total_pl_privado']:     
-            limite = row['valor_alocacao_inicial']+ row[col]
-            constraints.append({'type': 'ineq', 'fun': lambda x,i = index,l = limite: l-x[i]})
-            limite_fundo.append(limite)
-        limites[row['fundo']] = limite_fundo
+        limite = limites_maximos[i]
+        #constraints.append({'type': 'ineq', 'fun': lambda x,i=i: alocacao_maiorque_0(x,i)})
+        constraints.append({'type':'ineq','fun':lambda x,i=i:limite_maximo(x,i,limites_maximos[i])})
 
     #Resolver o problema de minimização
     minimizador = scipy.optimize.minimize(
@@ -51,9 +59,12 @@ def otimiza_regua(regua_ideal:pd.DataFrame, diferencas:pd.DataFrame)->tuple[pd.D
         x0=x0,
         args=(regua_ideal),
         constraints=constraints,
+        bounds= bounds,
         method='SLSQP',
-        options={'disp':False}
+        options={'maxiter': 100000,'disp':True}
     )    
+
+    print(minimizador.success)
     erro = minimizador.fun
     regua_otimizada = minimizador.x
 
