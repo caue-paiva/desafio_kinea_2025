976bbed353c223e0af6f4916263b9268b014ab66
“Caue <cauepaivalira@outlook.com>
2025-02-21 18:15:20 -0300

    FEAT1: Começo da implementação do PArser da ordem final

---
+++ AplicacaoFinal/ParserOrdemFinal.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from datetime import datetime
+
+def get_ordem_original()->pd.DataFrame:
+   return pd.read_csv("ordem_2025-02-21 17_37_33.csv")
+
+def get_input_nexxus_final()->pd.DataFrame:
+   return pd.read_csv("input_nexxus_teste_joao (2).csv",sep=";")
+
+
+ordem_df = get_ordem_original()
+enquadrado_nexxus_df = get_input_nexxus_final()
+
+
+enquadrado_nexxus_df = enquadrado_nexxus_df.merge( #Join pelo ativo e o preço ser igual
+   ordem_df,left_on=["ATIVO","PU"],right_on=["Ticker","Price"], how="inner"
+)
+
+enquadrado_nexxus_df =  enquadrado_nexxus_df.drop(["ATIVO","QTDE","PU"],axis=1)
+
+
+data = datetime.today().date()
+
+enquadrado_nexxus_df["data"] = data
+enquadrado_nexxus_df["Data Liq."] = data
+enquadrado_nexxus_df["Data Pay."] = data
+enquadrado_nexxus_df["Conta Mae Allocation"] = "Conta Mae Allocation"
+enquadrado_nexxus_df["TRADER"] = "Guilherme Ali Abdallah Bassani"
+enquadrado_nexxus_df["S. Broker"] = enquadrado_nexxus_df["Broker"]
+enquadrado_nexxus_df["1"] = None
+enquadrado_nexxus_df["1"] = None
+enquadrado_nexxus_df["1"] = None
+enquadrado_nexxus_df["1"] = None
+
+
+enquadrado_nexxus_df = enquadrado_nexxus_df.rename(
+   {
+      "FUNDO": "Allocation",
+   }, axis = 1)
+
+print(enquadrado_nexxus_df.head())
