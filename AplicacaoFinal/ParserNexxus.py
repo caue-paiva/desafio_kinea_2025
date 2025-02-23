6eca4e6afc07788397af031003dbbf309456a59e
caue-paiva <caue-paiva>
2025-02-23 18:14:34 +0000

    "Change1: Mudei parser das reguas para o arquivo nexxus que o ali vai baixar"

--- AplicacaoFinal/ParserNexxus.py
+++ AplicacaoFinal/ParserNexxus.py
@@ -18,18 +18,10 @@ class ParserNexxus:
         """
         Agrega todas as réguas no diretório ReguasOtimizadas em um DF único
         """
-        df_agregado = pd.DataFrame()
-        for file in self.__PATH_REGUAS.glob("*.csv"): #todos os arquivos .csv
-            nome_arquivo = str(file)
-            ativo:str = nome_arquivo.split("_")[-1].split(".")[0] #pega string depois do _ e antes do .
-            df = pd.read_csv(file)
-            df["Product"] = ativo
-            df_agregado = pd.concat([df_agregado, df])
-         
+        path = Path("/Volumes/desafio_kinea/boletagem_cp/files/InputNexxus/input_nexxus.csv")
+        return pd.read_csv(path)
 
-        return df_agregado
-
-    def regua_para_nexxus(self, nome_arquivo_finalizado:str = "input_nexxus_teste.csv")->None:
+    def regua_para_nexxus(self, nome_arquivo_finalizado:str = "input_nexxus_ali.csv")->None:
         """
         Agrega todos os arquivos de réguas otimizadas no diretório ReguasOtimizadas do volume em um único CSV no formato que o nexxus requer. Salva o arquivo de input para o nexxus no diretório
         InputNexxus do volume.
@@ -42,17 +34,21 @@ class ParserNexxus:
         reguas_agregadas:pd.DataFrame = self.__agrega_reguas()
         data_atual = datetime.today().date()
         reguas_agregadas["trade_date"] = data_atual #coluna da data da trade
-        reguas_agregadas["financial_price"] = reguas_agregadas["preco"] * reguas_agregadas["quantidade"]
 
         reguas_agregadas = reguas_agregadas.rename({ #renomeia colunas
-            "fundo":"trading_desk",
-            "preco":"price",
-            "quantidade": "amount",
+            "FUNDO":"trading_desk",
+            "PU":"price",
+            "QTDE": "amount",
+            "ATIVO": "Product",
+            "VALOR": "financial_price"
         },axis=1)
 
         df_classe_produtos = self.__DADOS.get_classe_produto() #df que mapeia cada ativo/produto com sua classe
         reguas_agregadas = pd.merge(reguas_agregadas, df_classe_produtos, on="Product") #inner join pelo produto, para trazer a classe
-        reguas_agregadas = reguas_agregadas.rename({"ProductClass":"product_class"},axis=1)
+        reguas_agregadas = reguas_agregadas.rename({"ProductClass":"product_class","Product":"product"},axis=1)
+
+        
+        reguas_agregadas = reguas_agregadas.drop(columns=["Unnamed: 0"],axis=1)
         reguas_agregadas.to_csv(self.__PATH_INPUT_NEXXUS / nome_arquivo_finalizado, index=False) #salva no diretório de inputs para o Nexxus
 
 
@@ -65,4 +61,4 @@ class ParserNexxus:
 
 if __name__ == "__main__":
     parser = ParserNexxus()
-    parser.regua_para_nexxus("Oi.csv")
+    parser.regua_para_nexxus()
